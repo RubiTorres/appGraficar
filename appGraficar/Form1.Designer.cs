4b137b249b5c58d3c10e08137c3a05c892c798b3
RUBI ESMERALDA TORRES ARMENDARIZ <I21050591@monclova.tecnm.mx>
2023-05-02 01:06:34 -0600

    Agregar archivos de proyecto.

---
+++ appGraficar/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace appGraficar
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Variable del diseñador necesaria.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Limpiar los recursos que se estén usando.
+    /// </summary>
+    /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Código generado por el Diseñador de Windows Forms
+
+    /// <summary>
+    /// Método necesario para admitir el Diseñador. No se puede modificar
+    /// el contenido de este método con el editor de código.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+      System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+      System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+      this.btnExaminar = new System.Windows.Forms.Button();
+      this.lstvDatos = new System.Windows.Forms.ListView();
+      this.tvJerarquia = new System.Windows.Forms.TreeView();
+      ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // chart1
+      // 
+      this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+      chartArea1.AxisX.IntervalOffset = 1D;
+      chartArea1.Name = "ChartArea1";
+      this.chart1.ChartAreas.Add(chartArea1);
+      legend1.Name = "Legend1";
+      this.chart1.Legends.Add(legend1);
+      this.chart1.Location = new System.Drawing.Point(744, 41);
+      this.chart1.Name = "chart1";
+      series1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.VerticalCenter;
+      series1.ChartArea = "ChartArea1";
+      series1.IsXValueIndexed = true;
+      series1.Legend = "Legend1";
+      series1.Name = "S";
+      this.chart1.Series.Add(series1);
+      this.chart1.Size = new System.Drawing.Size(410, 463);
+      this.chart1.TabIndex = 0;
+      this.chart1.Text = "chart1";
+      this.chart1.Visible = false;
+      // 
+      // btnExaminar
+      // 
+      this.btnExaminar.Location = new System.Drawing.Point(12, 12);
+      this.btnExaminar.Name = "btnExaminar";
+      this.btnExaminar.Size = new System.Drawing.Size(72, 26);
+      this.btnExaminar.TabIndex = 1;
+      this.btnExaminar.Text = "Examinar";
+      this.btnExaminar.UseVisualStyleBackColor = true;
+      this.btnExaminar.Click += new System.EventHandler(this.btnExaminar_Click);
+      // 
+      // lstvDatos
+      // 
+      this.lstvDatos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.lstvDatos.HideSelection = false;
+      this.lstvDatos.Location = new System.Drawing.Point(300, 41);
+      this.lstvDatos.Name = "lstvDatos";
+      this.lstvDatos.Size = new System.Drawing.Size(438, 463);
+      this.lstvDatos.TabIndex = 2;
+      this.lstvDatos.UseCompatibleStateImageBehavior = false;
+      this.lstvDatos.Visible = false;
+      // 
+      // tvJerarquia
+      // 
+      this.tvJerarquia.Location = new System.Drawing.Point(0, 44);
+      this.tvJerarquia.Name = "tvJerarquia";
+      this.tvJerarquia.Size = new System.Drawing.Size(259, 460);
+      this.tvJerarquia.TabIndex = 3;
+      this.tvJerarquia.Visible = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1155, 516);
+      this.Controls.Add(this.tvJerarquia);
+      this.Controls.Add(this.lstvDatos);
+      this.Controls.Add(this.btnExaminar);
+      this.Controls.Add(this.chart1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnExaminar;
+    private System.Windows.Forms.ListView lstvDatos;
+    private System.Windows.Forms.TreeView tvJerarquia;
+  }
+}
+
---
+++ appGraficar/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace appGraficar
+{
+  public partial class Form1 : Form
+  {
+    OpenFileDialog dialogo = new OpenFileDialog();
+    public Form1()
+    {
+      InitializeComponent();
+      lstvDatos.View = View.Details;
+    }
+    private void btnExaminar_Click(object sender, EventArgs e)
+    {
+      //listview
+
+      if (dialogo.ShowDialog() != DialogResult.OK)
+      {
+        return;
+      }
+      tvJerarquia.Visible = true;
+      chart1.Visible = true;
+      lstvDatos.Visible = true;
+      lstvDatos.Clear();
+      string rutaArchivo = dialogo.FileName;
+      StreamReader sr = new StreamReader(rutaArchivo, Encoding.GetEncoding(1252));
+      string columnas = sr.ReadLine();
+      string[] columna = columnas.Split('|');
+      for (int i = 0; i < columna.Length; i++)
+      {
+        lstvDatos.Columns.Add(columna[i]);
+      }
+      string renglon;
+      while ((renglon = sr.ReadLine()) != null)
+      {
+        string[] datos = renglon.Split('|');
+        ListViewItem item = new ListViewItem(datos[0]);
+        for (int i = 1; i < datos.Length; i++)
+        {
+          item.SubItems.Add(datos[i]);
+        }
+        lstvDatos.Items.Add(item);
+      }
+
+      //graficar
+
+      StreamReader dr = new StreamReader(rutaArchivo, Encoding.GetEncoding(1252));
+      string x = "";
+      int y = 0;
+      while ((renglon=dr.ReadLine()) != null)
+      {
+        string[] datos = renglon.Split('|');
+        if (x != datos[0] && x !="")
+        {
+          chart1.Series[0].Points.AddXY(x, y);
+          y = 0;
+        }
+        x = datos[0];
+        y++;
+      }
+      chart1.Series[0].Points.AddXY(x, y);
+      sr.Close();
+
+      //jerarquia
+      //LlenarArbol(sr);
+    }
+    private void LlenarArbol(StreamReader sr)
+    {
+      string renglon;
+      string x = "";
+      TreeNode ciudad = new TreeNode();
+      TreeNode estado = new TreeNode();
+      TreeNode codigoPostal = new TreeNode();
+      TreeNode colonia = new TreeNode();
+      string cd = "";
+      string cp = "";
+      while ((renglon = sr.ReadLine()) != null)
+      {
+        string[] datos = renglon.Split('|');
+        if (colonia.Text != datos[1] && x != "")
+        {
+          if (codigoPostal.Text != datos[0] && ciudad.Text != "")
+          {
+            if (ciudad.Text != datos[5] && ciudad.Text != "")
+            {
+              if (estado.Text != datos[4] && estado.Text != "")
+              {
+                tvJerarquia.Nodes.Add(estado.Text);
+                estado = new TreeNode();
+              }
+            }
+            estado.Text = datos[5];
+            estado.Nodes.Add(ciudad);
+            ciudad = new TreeNode();
+          }
+          ciudad.Text = datos[1];
+          codigoPostal.Nodes.Add(ciudad);
+          codigoPostal = new TreeNode();
+        }
+        codigoPostal.Text = datos[5];
+        codigoPostal.Nodes.Add(codigoPostal);
+        colonia.Text = datos[0];
+
+      }
+    }
+
+  }
+}
